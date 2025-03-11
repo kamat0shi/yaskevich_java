fc7fff600943a4dcc1f1ee71bd44d7ad147f8039
Waaanther <nnelkane@gmail.com>
2025-03-11 11:21:41 +0300

    fix problems

--- src/main/java/com/example/rest_service/RestControllerExample.java
+++ src/main/java/com/example/rest_service/RestControllerExample.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/api")
